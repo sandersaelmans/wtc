90afedf613cf7117e491736287f399b3f3fbd641
Sander Saelmans <sander.saelmans@sogeti.com>
2018-06-04 23:03:58 +0200

    added base project

---
+++ wtc/wtc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wtc
+//
+//  Created by Sander Saelmans on 04/06/2018.
+//  Copyright © 2018 Sander Saelmans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
